2939f2a82205bfc6c65b2076344178cc9171dbb0
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-04-29 16:52:48 -0700

    Sending Assignment 3 cover.php

---
+++ assignment-03/cover.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sports Covedrs</title>
+
+
+</head>
+
+<body>
+
+<li>
+<img src = "http://ecx.images-amazon.com/images/I/41KpZ99ymlL._BO2,204,203,200_PIsitb-sticker-v3-big,TopRight,0,-55_SX278_SY278_PIkin4,BottomRight,1,22_AA300_SH20_OU01_.jpg">
+<strong> <?= $activities["sports"] ?></strong>
+</li>
+</body>
+</html>
