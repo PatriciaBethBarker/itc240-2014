471ff2c0302a4eb911a234fe89ce7a9ebc4b5954
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-05-20 12:36:38 -0700

    revised insert file

--- assignment-05/insert.php
+++ assignment-05/insert.php
@@ -5,12 +5,12 @@
 		$name=$_POST['name'];
 		$type=$_POST['type'];
 		$payment_type=$_POST['payment_type'];
-		$expense_type=$_POST['expense_type'];
+		$expense_type=$_POST['expense_date'];
 		$amount=$_POST['amount']; 
 		$total=$_POST['total']; 
 mysqlli_connect(localhost,$username,$password);
 mysqlli_select_db($database) or die ("Unable to select database");
-	$query = "INSERT INTO expense VALUES('','$name','$type','$payment_type', '$expense_type','$amount', '$total')";
+	$query = "INSERT INTO expense VALUES('','$name','$type','$payment_type', '$expense_date','$amount', '$total')";
 mysql_query($query);
 mysql_close(); 
 ?>
