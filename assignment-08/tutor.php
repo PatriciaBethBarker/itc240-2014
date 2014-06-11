ac2362e9dc2ade12dcd8c41ae6a1545a3076d175
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-06-10 19:03:42 -0700

    more changes

--- assignment-08/tutor.php
+++ assignment-08/tutor.php
@@ -23,20 +23,3 @@
   }
 
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Tutor Listing</title>
-</head>
-
-<body>
-
-
-
-
-</body>
-</html>
