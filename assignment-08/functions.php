2201b57e4df2e0b0effc107edb88940588144b45
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-06-10 19:06:26 -0700

    new changes

--- assignment-08/functions.php
+++ assignment-08/functions.php
@@ -18,47 +18,9 @@ $mysql = new mysqli(
 function update_tutorDate($stName, $stID){
   global $mysql;
   $prepared = $mysql->prepare('UPDATE tutorList SET tutorDate = ?, WHERE stName =? ');
-  $prepared->bind_param("", $tutorDate, $stName);
+  $prepared->bind_param("ss", $tutorDate, $stName);
   $prepared->execute;
   
 	
 }
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Function file for Assignment 8</title>
-</head>
-
-<body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<ul>
-		<li>
-		<li>
-		<li>
-		<li>
-	</ul>
-
-
-
-
-</body>
-</html>
