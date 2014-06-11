c70045e68e10029bcbefe0bfe23fadd221f364e7
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-06-10 18:43:15 -0700

    still one more change

--- assignment-07/cover.php
+++ assignment-07/cover.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Library Book Covers</title>
-</head>
-
-<body>
-
-	<ul>
-		<li><img src = "http://www.syndetics.com/index.aspx?isbn=9781451660319/MC.GIF&client=sepup&type=xw12&oclc="><strong> <?= $sfbook["cover"] ?></strong></li>
-	</ul>
-
-</body>
-</html>
+		<li><img src="<?= $sfbook["cover"] ?>"></li>
+<!--only repeat the list item-->
