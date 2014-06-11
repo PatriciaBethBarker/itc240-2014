d4ed65e4ea810f46aa0954a70955c5c941aa2bb6
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-06-10 18:42:39 -0700

    more changes

--- assignment-07/book.php
+++ assignment-07/book.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Book list in the Library</title>
-
-
-</head>
-
-<body>
 <!-- A choice between 2 "views" -->
 <!-- 1.  A "cover" view with the book cover art, title, author name and a checkout button -->
 <!-- 2.  A "listing" view with book cover art, title, author name, a short description and a checkout button -->
-
-<h2>Listing Views</h2>
-	<ul>
-		<li><strong><?= $sfbook["cover"] ?></strong>
+<!-- this is the part that repeats, it doesn't need the html-->
+		<li><img src="<?= $sfbook["cover"] ?>"></strong>
 		<li><em><?= $sfbook["description"] ?></em>
